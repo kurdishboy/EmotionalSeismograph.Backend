4bfadb42b0f787d90757a6cf3f274b820d0ff033
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-06-30 19:32:13 +0200

    chore: Add Identity Server - Implement Identity Options with defaults

--- Api/DependencyRegistrations/MicrosoftIdentity.cs
+++ Api/DependencyRegistrations/MicrosoftIdentity.cs
@@ -1,5 +1,6 @@
 using Domain.Models;
 using Infrastructure;
+using Microsoft.AspNetCore.Identity;
 
 namespace Api.DependencyRegistrations;
 
@@ -11,6 +12,31 @@ public static class MicrosoftIdentity
 
         services.AddIdentityApiEndpoints<User>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
+        
+        services.Configure<IdentityOptions>(options =>
+        {
+            // Default Lockout settings.
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+            
+            // Default Password settings.
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+            
+            // Default SignIn settings.
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+            
+            // Default User settings.
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = false;
+        });
 
         return services;
     }
