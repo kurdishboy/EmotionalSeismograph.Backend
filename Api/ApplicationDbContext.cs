c7f34a2092f69f2e2cd9d1a712b7d496f911af2e
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-06-30 16:16:48 +0200

    chore: Add Identity Server - Change default table names

--- Api/ApplicationDbContext.cs
+++ Api/ApplicationDbContext.cs
@@ -1,8 +1,50 @@
 using Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : IdentityDbContext<User, Role, long>(options);
+    : IdentityDbContext<User, Role, long>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<User>(b =>
+        {
+            b.ToTable("Users");
+        });
+
+        builder.Entity<IdentityUserClaim<long>>(b =>
+        {
+            b.ToTable("UserClaims");
+        });
+
+        builder.Entity<IdentityUserLogin<long>>(b =>
+        {
+            b.ToTable("UserLogins");
+        });
+
+        builder.Entity<IdentityUserToken<long>>(b =>
+        {
+            b.ToTable("UserTokens");
+        });
+
+        builder.Entity<Role>(b =>
+        {
+            b.ToTable("Roles");
+        });
+
+        builder.Entity<IdentityRoleClaim<long>>(b =>
+        {
+            b.ToTable("RoleClaims");
+        });
+
+        builder.Entity<IdentityUserRole<long>>(b =>
+        {
+            b.ToTable("UserRoles");
+        });
+    }
+}
