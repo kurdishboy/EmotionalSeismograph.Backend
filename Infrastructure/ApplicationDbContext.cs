d8f9009be1a6df7ee5a9b0ff7c9d04916ea31aa0
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-22 12:54:53 +0200

    chore: Implement IUnitOfWork

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 using System.Reflection;
+using Application.Abstractions;
 using Domain.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : IdentityDbContext<User, Role, long>(options)
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<User, Role, long>(options), IUnitOfWork
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
