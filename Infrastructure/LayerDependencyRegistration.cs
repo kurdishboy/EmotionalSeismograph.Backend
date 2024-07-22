39dded6881116d710fafe724e4f812f33063e831
Arman Fatemi <s.arman.fatemi@outlook.com>
2024-07-22 15:28:12 +0200

    chore: Move microsoft identity registerer service

--- Infrastructure/LayerDependencyRegistration.cs
+++ Infrastructure/LayerDependencyRegistration.cs
@@ -1,14 +1,79 @@
+using Application.Abstractions;
+using Domain.Models;
+using Domain.Repositories;
 using Infrastructure.DependencyRegistrations;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class LayerDependencyRegistration
 {
-    public static IServiceCollection UseInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddMicrosoftEntityFramework();
+        services.AddMicrosoftIdentity();
+        
+        // Add repositories
+        services.AddScoped<IEmotionsRepository, EmotionRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
         return services;
+    }
+    
+    public static void UseInfrastructure(this WebApplication? app)
+    {
+        if (app is null) return;
+
+        UseMicrosoftIdentity(app);
+    }
+
+    private static IServiceCollection AddMicrosoftIdentity(this IServiceCollection services)
+    {
+        services.AddAuthorization();
+
+        services.AddIdentityApiEndpoints<User>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            // Default Lockout settings.
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+
+            // Default Password settings.
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+
+            // Default SignIn settings.
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Default User settings.
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = false;
+        });
+
+        return services;
+    }
+
+    private static WebApplication? UseMicrosoftIdentity(this WebApplication? app)
+    {
+        if (app is null) return app;
+
+        app.UseAuthorization();
+        app.MapIdentityApi<User>();
+
+        return app;
     }
 }
